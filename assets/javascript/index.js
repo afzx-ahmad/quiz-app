9e9ac9211006c14f0b36e10b68f1411d671e90c1
afzx-ahmad <afzx.ahmad@gmail.com>
2021-12-11 09:10:29 +0700

    Creating First UI Main Page

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,12 @@
 window.addEventListener('load', (event) => {
-    console.log('Hello World');
+    var startButton = document.getElementsByClassName("start-button");
+    var quizFooter = document.querySelector(".quiz-footer");
+
+    startButton[0].addEventListener("mouseenter", function () {
+        quizFooter.style.backgroundColor = "#1FD9FF";
+    });
+
+    startButton[0].addEventListener("mouseleave", () => {
+        quizFooter.removeAttribute("style");
+    });
 });
